cb78d1eb27e45087d7969d6e9b86001f9f894732
masteryi-0018 <1536474741@qq.com>
2026-07-26 03:26:00 +0000

    demo from hf: https://huggingface.co/docs/diffusers/main/en/api/pipelines/controlnet_sdxl#diffusers.StableDiffusionXLControlNetImg2ImgPipeline

---
+++ main.py
@@ -0,0 +1,74 @@
+# pip install accelerate transformers safetensors diffusers
+
+import os
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+os.environ["HF_HOME"] = os.path.join(os.path.dirname(__file__), "models")
+
+import torch
+import numpy as np
+from PIL import Image
+
+from transformers import DPTImageProcessor, DPTForDepthEstimation
+from diffusers import ControlNetModel, StableDiffusionXLControlNetImg2ImgPipeline, AutoencoderKL
+from diffusers.utils import load_image
+
+depth_estimator = DPTForDepthEstimation.from_pretrained("Intel/dpt-hybrid-midas").to("cuda")
+feature_extractor = DPTImageProcessor.from_pretrained("Intel/dpt-hybrid-midas")
+controlnet = ControlNetModel.from_pretrained(
+    "diffusers/controlnet-depth-sdxl-1.0-small",
+    variant="fp16",
+    use_safetensors=True,
+    torch_dtype=torch.float16,
+)
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
+pipe = StableDiffusionXLControlNetImg2ImgPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    controlnet=controlnet,
+    vae=vae,
+    variant="fp16",
+    use_safetensors=True,
+    torch_dtype=torch.float16,
+)
+pipe.enable_model_cpu_offload()
+
+
+def get_depth_map(image):
+    image = feature_extractor(images=image, return_tensors="pt").pixel_values.to("cuda")
+    with torch.no_grad(), torch.autocast("cuda"):
+        depth_map = depth_estimator(image).predicted_depth
+
+    depth_map = torch.nn.functional.interpolate(
+        depth_map.unsqueeze(1),
+        size=(1024, 1024),
+        mode="bicubic",
+        align_corners=False,
+    )
+    depth_min = torch.amin(depth_map, dim=[1, 2, 3], keepdim=True)
+    depth_max = torch.amax(depth_map, dim=[1, 2, 3], keepdim=True)
+    depth_map = (depth_map - depth_min) / (depth_max - depth_min)
+    image = torch.cat([depth_map] * 3, dim=1)
+    image = image.permute(0, 2, 3, 1).cpu().numpy()[0]
+    image = Image.fromarray((image * 255.0).clip(0, 255).astype(np.uint8))
+    return image
+
+
+prompt = "A robot, 4k photo"
+images_dir = os.path.join(os.path.dirname(__file__), "images")
+os.makedirs(images_dir, exist_ok=True)
+image = load_image(
+    "https://hf-mirror.com/datasets/hf-internal-testing/diffusers-images/resolve/main"
+    "/kandinsky/cat.png"
+).resize((1024, 1024))
+image.save(os.path.join(images_dir, "cat.png"))
+controlnet_conditioning_scale = 0.5  # recommended for good generalization
+depth_image = get_depth_map(image)
+
+images = pipe(
+    prompt,
+    image=image,
+    control_image=depth_image,
+    strength=0.99,
+    num_inference_steps=50,
+    controlnet_conditioning_scale=controlnet_conditioning_scale,
+).images
+images[0].save(f"robot_cat.png")
